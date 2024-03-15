272214a3aa02427367a7e2e61baf42eaf618256f
Tanishq Jain <Tanishq.J_INT1581@accolite.com>
2024-03-15 15:03:18 +0530

    initial commit

---
+++ src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+    {
+        id: "1",
+        Name: "Tanishq",
+        Age: "21",
+    },
+    {
+        id: "2",
+        Name: "Ajay",
+        Age: "22",
+    },
+    {
+        id: "3",
+        Name: "Ritik",
+        Age: "23",
+    },
+];
+
+export default array;
